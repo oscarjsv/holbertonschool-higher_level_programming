528361147c0e42da06db98b44c7d104b26acd0da
oscarjsv <gumball_junior@hotmail.com>
2020-09-01 13:23:27 -0500

    ejercicios

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,15 @@
 import random
 #!/usr/bin/python3
 number = random.randint(-10000, 10000)
-if number > 0:
-    last_D = number % 10
+if number < 0:
+    last = number % -10
 else:
-    last_D = (number % 10) * -1
-if last_D > 5:
-    print('Last digit of {:d} is {:d} and is greater than 5'. format(
-        number, last_D))
-elif last_D == 0:
-    print('Last digit of {:d} is {:d} and is 0'. format(number, last_D))
-else:
-    print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(
-        number, last_D))
+    last = number % 10
+    
+print('Last digit of', end=" ")
+if last > 5:
+    print("{} is {} and is greater than 5".format(number, last))
+elif last == 0:
+    print("{} is {} and is 0".format(number, last))
+elif last < 6 and last != 0:
+    print("{} is {} and is less than 6 and not 0".format(number, last))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for number in range(00, 99):
     print("{:02d}".format(number), end=(', '))
-    if number == 98:
-        print(99)
-    
+print(99)
