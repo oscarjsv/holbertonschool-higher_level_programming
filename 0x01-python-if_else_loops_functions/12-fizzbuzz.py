1af64761da0275581d97bb3568abd24a6738f629
oscarjsv <gumball_junior@hotmail.com>
2020-09-01 16:09:49 -0500

    ejercicios

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 != 0 and number % 5 != 0:
+            print(number, end="")
+        if number % 3 == 0:
+            print("Fizz", end="")
+        if number % 5 == 0:
+            print("Buzz", end="")
+        if number != 101:
+            print(" ", end="")
