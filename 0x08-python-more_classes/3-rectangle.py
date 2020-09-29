9b49117255519b617f1a5c682f9ac0c6bd447b94
oscarjsv <gumball_junior@hotmail.com>
2020-09-28 20:49:12 -0500

    number 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,4 +54,4 @@ class Rectangle:
 
     def __str__(self):
         ''' str metodo '''
-        return (("#" * self.width + "\n") * self.height)[:-1]
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
