68bf86ae94e563c07f351af7b715fce6b3d0c1dc
oscarjsv <gumball_junior@hotmail.com>
2020-09-28 20:40:20 -0500

    number 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -48,4 +48,5 @@ class Rectangle:
             raise TypeError('height must be an integer')
         elif value < 0:
             raise ValueError('heigth must be >= 0')
+        
         self.__height = value
