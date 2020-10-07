3d57cc758e571811af05193e5f0cdb28752a68e9
oscarjsv <gumball_junior@hotmail.com>
2020-10-06 19:50:11 -0500

    all

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains a class
+with public instance and Raises
+exception when required
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class that inherits from rectangle
+    Arguments:
+        Rectangle {class} -- Class rectangle
+    """
+    def __init__(self, size):
+        """ Square class that inherits from rectangle
+        Arguments:
+            size {int} -- size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-''' base geometry 8'''
+"""
+This module contains a class
+with public instance and Raises
+exception when required
+"""
 
-
-class BaseGeometry:
-    ''' base geometry '''
-
-    def area(self):
-        ''' method area'''
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        ''' integer validator '''
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        elif value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
