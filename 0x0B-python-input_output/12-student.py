3bf2031ecbc07fd08b31ece918464e1ed10c4629
oscarjsv <gumball_junior@hotmail.com>
2020-10-08 16:34:22 -0500

    12-student.py

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -12,12 +12,14 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """to_json
         """
-        dic = {}
+        class object type
+        """
         if attrs is None:
             return self.__dict__
-        for i in attrs:
-            if hasattr(self, i):
-                dic[i] = getattr(self, i)
-        return dic
+        else:
+            new_dict = {}
+            for i in self.__dict__:
+                if i in attrs:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
