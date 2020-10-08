d765aaf3e5a24e94eb48fa6c8b8c470f76c3a3b1
oscarjsv <gumball_junior@hotmail.com>
2020-10-08 11:07:22 -0500

    ejercicios

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 ''' write the line '''
 
+
 def write_file(filename="", text=""):
     ''' write file '''
+
+    with open(filename, 'w', encoding='utf-8') as writer:
+        return writer.write(text)
