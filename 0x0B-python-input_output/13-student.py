a83c7622b6f84987135d50c7ed91d31b4c4ce492
oscarjsv <gumball_junior@hotmail.com>
2020-10-08 16:44:15 -0500

    13-student.py

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-''' 11 student '''
+"""Student class
+Returns:
+    json -- dictionary representation of a Student instance
+"""
 
 
 class Student:
-    ''' class estudent '''
+    """Class Student
+    """
 
     def __init__(self, first_name, last_name, age):
-        ''' method  built-in '''
+        """initialize method
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """to_json
         """
-        class object type
-        """
+        dic = {}
         if attrs is None:
             return self.__dict__
-        else:
-            new_dict = {}
-            for i in self.__dict__:
-                if i in attrs:
-                    new_dict[i] = self.__dict__[i]
-            return new_dict
-    
+        for i in attrs:
+            if hasattr(self, i):
+                dic[i] = getattr(self, i)
+        return dic
+
     def reload_from_json(self, json):
-            for k, v in json.items():
-                if hasattr(self, k):
-                    setattr(self, k, v)
+        for k, v in json.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
