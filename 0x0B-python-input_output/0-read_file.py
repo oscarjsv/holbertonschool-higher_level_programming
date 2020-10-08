a5ff53f29c84237ea77105c83fe58315ec661b02
oscarjsv <gumball_junior@hotmail.com>
2020-10-08 10:56:05 -0500

    ejercicios

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     ''' read file '''
-    with open('my_file_0.txt', encoding='utf-8') as reader:
+    with open(filename, encoding='utf-8') as reader:
         print(reader.read())
--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,5 +4,5 @@
 
 def number_of_lines(filename=""):
     ''' number of lines '''
-    with open('my_file_0.txt', 'r', encoding='utf-8') as read_l:
+    with open(filename, 'r', encoding='utf-8') as read_l:
         return len(read_l.readlines())
