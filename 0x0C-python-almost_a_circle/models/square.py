d37f55a2945c36772cd7e209afb07448d0c39a5b
oscarjsv <gumball_junior@hotmail.com>
2020-10-13 16:26:51 -0500

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' module Square '''
+
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    ''' class square '''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        ''' method built square'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return ("[{}] ({}) {}/{} - {}".
+                format(__class__.__name__, self.id, self.x, self.y, self.height))
