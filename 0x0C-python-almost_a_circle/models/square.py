a3d90081cc422411500c7d3c144c640ce5ce5d59
oscarjsv <gumball_junior@hotmail.com>
2020-10-13 16:27:37 -0500

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,4 +13,5 @@ class Square(Rectangle):
 
     def __str__(self):
         return ("[{}] ({}) {}/{} - {}".
-                format(__class__.__name__, self.id, self.x, self.y, self.height))
+                format(__class__.__name__,
+                       self.id, self.x, self.y, self.height))
