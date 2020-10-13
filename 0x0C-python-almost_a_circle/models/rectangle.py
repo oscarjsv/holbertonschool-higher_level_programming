fca37c0abc4a9b3090aa2830b3003b4991476f43
oscarjsv <gumball_junior@hotmail.com>
2020-10-13 18:21:25 -0500

    12 rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,3 +113,8 @@ class Rectangle(Base):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        ''' to dictionary '''
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width}
