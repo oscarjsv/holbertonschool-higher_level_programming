70b7c2bde73f758c08ea838d4f81337c2b76fcaa
oscarjsv <gumball_junior@hotmail.com>
2020-11-19 12:57:48 -0500

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE
 IF NOT EXISTS hbtn_0d_usa.cities                                                                                                        
 (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,                                                                                     
 state_id INT NOT NULL,                                                                                                                  
-FOREIGN KEY(state_id) REFERENCES                                                                                                        
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.cities(id),                                                                                
 name VARCHAR(256) NOT NULL);
