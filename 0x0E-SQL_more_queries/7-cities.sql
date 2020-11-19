bddb8f8199fe7746f5bf7590494e292d359b3bef
oscarjsv <gumball_junior@hotmail.com>
2020-11-19 13:14:45 -0500

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
---create table and database
-CREATE DATABASE                                                                                                                         
-IF NOT EXISTS hbtn_0d_usa;                                                                                                              
-CREATE TABLE                                                                                                                            
-IF NOT EXISTS hbtn_0d_usa.cities(
-id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,                                                                                     
-state_id INT NOT NULL,                                                                                                                  
-FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),                                                                                
-name VARCHAR(256) NOT NULL);
+-- Create the database hbtn_0d_usa and the table cities
+--  state_id must be a foreign key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	UNIQUE (id),
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+);
