bb972b2c1e0d845aa1f0a3b471888eb2315785d2
oscarjsv <gumball_junior@hotmail.com>
2020-12-13 15:41:52 -0500

    correction line 21

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
 
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(argv[4]))
+        "SELECT * FROM states WHERE name\
+             LIKE '{}' ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
